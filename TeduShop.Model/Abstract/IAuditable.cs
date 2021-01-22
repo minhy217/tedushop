52ccff9b09b2fa068fc5a310ceb3418d7e23a3a9
myc217 <y.cm@grex-solutions.com>
2021-01-22 11:08:04 +0700

    import entities

---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreateDate { get; set; }
+        string CreateBy { get; set; }
+        DateTime? UpdateDate { get; set; }
+        string UpdateBy { get; set; }
+        string MetaKeyword { get; set; }
+        string MetaDecription { get; set; }
+        bool Status { get; set; }
+    }
+}
